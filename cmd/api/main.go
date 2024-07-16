2a39716a21e7abae1f579da6b1c7d648eae6f18a
Victor Navarro <victor.matias.navarro@gmail.com>
2024-07-15 19:35:47 -0700

    Finish chapter 11: graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 	"time"
 
@@ -68,6 +66,9 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+	// Defer a call to db.Close() so that the connection pool is closed.
+	defer db.Close()
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -75,25 +76,11 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
-
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	// Defer a call to db.Close() so that the connection pool is closed.
-	defer db.Close()
-
-	logger.Info("database connection pool established")
-
-	err = srv.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
 }
 
 // The openDB() function returns a sql.DB connection pool.
---
+++ cmd/api/server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	// Declare HTTP server using same settings as main() function
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+	shutdownError := make(chan error)
+
+	go func() {
+		// Create a quit channel which carries os.Signal values
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		app.logger.Info("caught signal", "signal", s.String())
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		// Call shutdown() on our server, passing the context we just made.
+		// Shutdown() will return nil if the graceful shutdown was successful,
+		// or an error (closing listeners fails, can't complete before deadline).
+		// We relay this return value to the shutdownError channel
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	// Likewise log a starting server message
+	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
+
+	// Calling Shutdown on our server will cause ListenAndServe to immediately return
+	// a http.ErrServerClosed error. If we see this, its actually a good thing and
+	// an indication the graceful shutdown has started.
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
+}
